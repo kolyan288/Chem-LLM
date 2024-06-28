b95c760035b777fbec4557c074b97dcfff555ff1
Kolyan288 <smoljanov.official@gmail.com>
2024-06-28 18:30:20 +0300

    6th commit

--- launcher.py
+++ launcher.py
@@ -1,7 +1,7 @@
 from AgentOllama_task import agentollama_task
 from AgentOllama_task_mode import agentollama_task_mode
 
-prompt = "Сделай нормальный пайплайн такую штуку короче молекулу классифицировать"
+prompt = "Я синтезировал молекулу и необходимо предсказать для неё свойства меры глобальной симметрии, меры структурной сложности, мерой липофильности молекулы"
 
 agentollama_task(prompt)
 agentollama_task_mode(prompt)
